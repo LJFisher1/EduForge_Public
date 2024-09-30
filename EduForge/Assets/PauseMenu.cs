aad097e6de332a5efc7a5d55f0f26dfe771ba670
jess bacchi <135135190+cheecsey@users.noreply.github.com>
2024-09-30 13:42:29 -0400

    settings now part of pausemenu
    settings button working as a ui feature and script is part of pause menu script

--- EduForge/Assets/PauseMenu.cs
+++ EduForge/Assets/PauseMenu.cs
@@ -1,25 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
     [SerializeField] GameObject PauseMenu;
     [SerializeField] GameObject PauseButton;
+    [SerializeField] GameObject SettingsUI;
 
-    public static string previousScene;
-
-    // Start is called before the first frame update
     public void Start()
     {
         PauseMenu.SetActive(false);
+        SettingsUI.SetActive(false);
     }
 
     public void Pause()
     {
         Time.timeScale = 0;
-
         PauseButton.SetActive(false);
         PauseMenu.SetActive(true);
     }
@@ -27,15 +24,37 @@ public class PauseMenuScript : MonoBehaviour
     public void Resume()
     {
         Time.timeScale = 1;
-
         PauseButton.SetActive(true);
         PauseMenu.SetActive(false);
     }
 
+    public void TogglePauseMenu()
+    {
+        bool isActive = PauseMenu.activeSelf;
+        PauseMenu.SetActive(!isActive);
+
+        if (!isActive) // If pause menu is activated
+        {
+            Time.timeScale = 0f;
+            PauseButton.SetActive(false);
+        }
+        else // If pause menu is deactivated
+        {
+            Time.timeScale = 1f;
+            PauseButton.SetActive(true);
+        }
+    }
+
     public void GoToSettings()
     {
-        previousScene = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene("Settings");
+        PauseMenu.SetActive(false);
+        SettingsUI.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        SettingsUI.SetActive(false);
+        PauseMenu.SetActive(true);
     }
 
     public void ExitGame()
