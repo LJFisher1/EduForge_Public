a9f66f7947b63165f5ebf3fcea2026e6c5477708
jess bacchi <135135190+cheecsey@users.noreply.github.com>
2024-10-14 18:27:33 -0400

    hint pop-up
    not fully functional yet

--- EduForge/Assets/Scripts/PauseMenu.cs
+++ EduForge/Assets/Scripts/PauseMenu.cs
@@ -11,12 +11,22 @@ public class PauseMenuScript : MonoBehaviour
     [SerializeField] GameObject SettingsUI;
     [SerializeField] Text hintCounterText;
     [SerializeField] GameObject hintCounterUI;
+    [SerializeField] GameObject HintPopUpUI;
+    [SerializeField] Text hintText;
 
     private int hintCounter = 5;
+    private string[] hints = {
+        "Hint Example One",
+        "Hint Example Two",
+        "Hint Example Three",
+        "Hint Example Four",
+        "Hint Example Five"
+    };
 
     public void Start()
     {
         PauseMenu.SetActive(false);
+        HintPopUpUI.SetActive(false);
         SettingsUI.SetActive(false);
         UpdateHintCounterUI();
         hintCounterUI.SetActive(true);
@@ -35,6 +45,7 @@ public class PauseMenuScript : MonoBehaviour
         Time.timeScale = 1;
         PauseButton.SetActive(true);
         PauseMenu.SetActive(false);
+        HintPopUpUI.SetActive(false);
         hintCounterUI.SetActive(true);
     }
 
@@ -53,6 +64,7 @@ public class PauseMenuScript : MonoBehaviour
         {
             Time.timeScale = 1f;
             PauseButton.SetActive(true);
+            HintPopUpUI.SetActive(false);
             hintCounterUI.SetActive(true);
         }
     }
@@ -69,6 +81,7 @@ public class PauseMenuScript : MonoBehaviour
         {
             hintCounter--;
             UpdateHintCounterUI();
+            DisplayHint();
         }
 
         Resume();
@@ -77,6 +90,22 @@ public class PauseMenuScript : MonoBehaviour
     private void UpdateHintCounterUI()
     {
         hintCounterText.text = hintCounter.ToString();
+    }
+
+    private void DisplayHint()
+    {
+        hintText.text = hints[5 - hintCounter - 1];
+        HintPopUpUI.SetActive(true);
+
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+        hintCounterUI.SetActive(true);
+        PauseButton.SetActive(true);
+    }
+
+    public void CloseHintPopUp()
+    {
+        HintPopUpUI.SetActive(false);
     }
 
     public void GoToSettings()
